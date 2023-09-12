c4901ba64e9d198fc6402b4edbc787f0a3d73efe
Gaurav Singh <gaurav165ongraph@gmail.com>
2023-09-12 19:01:21 +0530

    this commit for adding user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validate :validate_single_admin
+  validate :validate_single_admin, on: :create
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
   
   enum role: { admin: 'admin', student: 'student', tutor: 'tutor' ,manager: 'manager'}
   
 
+
+  # Other user model code
+
   private
-  
+
   def validate_single_admin
-    if User.where(role: 'admin').count > 1
-      errors.add(:role, 'Only one admin is allowed in the entire database.')
+    if role == 'admin'
+      admin_count = User.where(role: 'admin').count
+      if admin_count >= 1
+        errors.add(:role, 'Only one admin is allowed in the entire database.')
+      end
     end
   end
+  
 end
