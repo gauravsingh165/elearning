b0aa9efa7d8f6675e8dd1ee16497e8196e1baa5a
Gaurav Singh <gaurav165ongraph@gmail.com>
2023-09-13 17:32:37 +0530

    this commit for adding product_version controller

---
+++ app/controllers/product_versions_controller.rb
@@ -0,0 +1,37 @@
+class ProductVersionsController < ApplicationController
+  def new
+    @product_version = ProductVersion.new
+  end
+
+  def create
+    @product_version = ProductVersion.new(product_version_params)
+
+    if @product_version.save
+      redirect_to @product_version, notice: 'Product version was successfully created.'
+    else
+      render :new
+    end
+  end
+  def show
+    @product_version = ProductVersion.find_by(id: params[:id])
+  
+    if @product_version.nil?
+      flash[:alert] = 'Product version not found'
+      redirect_to product_versions_path # Redirect to index or any other page
+    end
+  end
+  def version_action
+    if current_user.product_version.name == 'Paid'
+    
+    elsif current_user.product_version.name == 'Free trial'
+     
+    else
+     
+    end
+  end
+  private
+
+  def product_version_params
+    params.require(:product_version).permit(:name, :features)
+  end
+end
