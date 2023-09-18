a413e930ad769986bf3b22e993a0b7c68e3e57af
Gaurav Singh <gaurav165ongraph@gmail.com>
2023-09-18 16:09:15 +0530

    this commit for adding ability.rb for fix the authorization for admin and other role for the user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,17 +2,38 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new 
+    user ||= User.new  # Guest user (not logged in)
 
     if user.admin?
       can :manage, :all
     elsif user.manager?
-      can :manage, [MCQ, Essay, Video, Textbook, User]
+      can :manage, [Mcq, Essay, Video, Textbook, User]
     elsif user.tutor?
-      can :manage, [MCQ, Essay]
-    elsif user.student?
-      can :read, [Course, ProductLine, ProductVersion]
-      can :manage, Enrollment, user_id: user.id
+      can :manage, [Mcq, Essay]
+    else
+      can :read, Video
+      can :read, Course
+      can :read, Essay
+      can :write, Essay
     end
   end
 end
+
+# class Ability
+#   include CanCan::Ability
+
+#   def initialize(user)
+#     user ||= User.new 
+
+#     if user.admin?
+#       can :manage, :all
+#     elsif user.manager?
+#       can :manage, [MCQ, Essay, Video, Textbook, User]
+#     elsif user.tutor?
+#       can :manage, [MCQ, Essay]
+#     elsif user.student?
+#       can :read, [Course, ProductLine, ProductVersion]
+#       can :manage, Enrollment, user_id: user.id
+#     end
+#   end
+# end
